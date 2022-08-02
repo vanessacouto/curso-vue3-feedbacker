663b08cc61f2af9a5234a90214e4990bd1600116
Vanessa <vanessa-almeida@prodesan.com.br>
2022-08-02 14:38:12 -0300

    criando modal factory

--- dashboard/src/components/ModalFactory/index.vue
+++ dashboard/src/components/ModalFactory/index.vue
@@ -2,41 +2,46 @@
   <teleport to="body">
     <div
       v-if="state.isActive"
-      class="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50"
-      @click="handleModalToogle({ status: false })"
+      class="
+        fixed
+        top-0
+        left-0
+        z-50
+        flex
+        items-center
+        justify-center
+        w-full
+        h-full
+        bg-black bg-opacity-50
+      "
+      @click="handleModalToogle({ status:false })"
     >
-      <div
-        class="fixed mx-10"
-        :class="state.width"
-        @click.stop
-      >
-        <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
-          <div class="flex flex-col px-12 py-10 bg-white">
+      <div class="fixed mx-10">
+        <div
+          class="
+            flex flex-col
+            overflow-hidden
+            bg-white
+            rounded-lg
+            animate__animated animate__fadeInDown animate__faster
+          "
+        >
+          <div class="flex flex-col px-12 py10 bg-white">
             <component :is="state.component" />
           </div>
         </div>
-
       </div>
     </div>
   </teleport>
 </template>
 
 <script>
-import { reactive, onMounted, onBeforeUnmount, defineAsyncComponent } from 'vue'
-import useModal from '../../hooks/useModal'
-
-const ModalLogin = defineAsyncComponent(() => import('../ModalLogin'))
-const ModalAccountCreate = defineAsyncComponent(() => import('../ModalCreateAccount'))
+import { reactive } from 'vue'
 
 const DEFAULT_WIDTH = 'w-3/4 lg:w-1/3'
 
 export default {
-  components: {
-    ModalLogin,
-    ModalAccountCreate
-  },
-  setup () {
-    const modal = useModal()
+  setup() {
     const state = reactive({
       isActive: false,
       component: {},
@@ -44,31 +49,12 @@ export default {
       width: DEFAULT_WIDTH
     })
 
-    onMounted(() => {
-      modal.listen(handleModalToogle)
-    })
+    function handleModalToogle ({ status }) {
 
-    onBeforeUnmount(() => {
-      modal.off(handleModalToogle)
-    })
-
-    function handleModalToogle (payload) {
-      if (payload.status) {
-        state.component = payload.component
-        state.props = payload.props
-        state.width = payload.width ?? DEFAULT_WIDTH
-      } else {
-        state.component = {}
-        state.props = {}
-        state.width = DEFAULT_WIDTH
-      }
-
-      state.isActive = payload.status
     }
 
     return {
-      state,
-      handleModalToogle
+      state
     }
   }
 }
