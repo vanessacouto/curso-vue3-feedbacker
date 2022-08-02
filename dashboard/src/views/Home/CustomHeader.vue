b18e72c8964bec56f7e80d4858169d2c82f85977
Vanessa <vanessa-almeida@prodesan.com.br>
2022-08-02 11:55:39 -0300

    criando a tela inicial

--- dashboard/src/views/Home/CustomHeader.vue
+++ dashboard/src/views/Home/CustomHeader.vue
@@ -3,21 +3,36 @@
     <div class="header-group">
       <div class="flex items-center justify-between py-10">
         <div class="w-28 lg:w-36">
-          <img class="w-full" src="../../assets/images/logo_white.png" alt="logo">
+          <img
+            class="w-full"
+            src="../../assets/images/logo_white.png"
+            alt="logo"
+          />
         </div>
 
         <div class="flex">
           <button
-            id="header-create-account-button"
-            @click="() => emit('create-account')"
-            class="px-6 py-2 font-bold rounded-full text-white focus:outline-none"
+            class="
+              px-6
+              py-2
+              font-bold
+              rounded-full
+              text-white
+              focus:outline-none
+            "
           >
             Crie uma conta
           </button>
           <button
-            id="header-login-button"
-            @click="() => emit('login')"
-            class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outline-none"
+            class="
+              px-6
+              py-2
+              font-bold
+              bg-white
+              rounded-full
+              text-brand-main
+              focus:outline-none
+            "
           >
             Entrar
           </button>
@@ -31,30 +46,32 @@
           felizes!
         </h1>
         <p class="text-lg font-medium text-white">
-          Receba ideias, reclamações e feedbacks com um <br class="hidden lg:inline-block">
-          simples widget na página.
+            Receba ideias, reclamações e feedbacks com um <br class="hidden lg:inline-block">
+            simples widget na página.
         </p>
         <div>
-          <button
-            @click="() => emit('create-account')"
-            id="cta-create-account-button"
-            class="px-6 py-2 mt-10 font-bold bg-white rounded-full text-brand-main focus:outline-none"
+            <button
+            class="
+              px-6
+              py-2
+              mt-10
+              font-bold
+              bg-white
+              rounded-full
+              text-brand-main
+              focus:outline-none
+            "
           >
             Crie uma conta grátis
           </button>
         </div>
       </div>
-
     </div>
   </header>
 </template>
 
 <script>
-export default {
-  setup (_, { emit }) {
-    return { emit }
-  }
-}
+export default {}
 </script>
 
 <style lang="postcss" scoped>
@@ -64,15 +81,15 @@ export default {
 }
 
 .header-group {
-  @apply flex flex-col w-4/5 max-w-6xl;
+    @apply flex flex-col w-4/5 max-w-6xl
 }
 
 @media (min-width: 640px) {
-  .header-group {
-    background-image: url(../../assets/images/blue_balloons.png);
-    background-size: 628px;
-    background-position: 90% 100%;
-    background-repeat: no-repeat;
-  }
+    .header-group {
+        background-image: url(../../assets/images/blue_balloons.png);
+        background-size: 628px;
+        background-position: 90% 100%;
+        background-repeat: no-repeat;
+    }
 }
 </style>
--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,11 +1,8 @@
 <template>
-  <custom-header
-    @create-account="handleAccountCreate"
-    @login="handleLogin"
-  />
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
   <contact />
   <div class="flex justify-center py-10 bg-brand-gray">
-    <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
+    <p class="font-medium text-center text-gray-800">feedbacker © 2022</p>
   </div>
 </template>
 
@@ -14,32 +11,23 @@ import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import CustomHeader from './CustomHeader.vue'
 import Contact from './Contact.vue'
-import useModal from '../../hooks/useModal'
 
 export default {
   components: { CustomHeader, Contact },
   setup () {
     const router = useRouter()
-    const modal = useModal()
-
     onMounted(() => {
+      // vai pegar o token do localStorage
       const token = window.localStorage.getItem('token')
       if (token) {
+        // se tiver um token envia o usuário para a rota de feedbacks
         router.push({ name: 'Feedbacks' })
       }
     })
 
-    function handleLogin () {
-      modal.open({
-        component: 'ModalLogin'
-      })
-    }
+    function handleLogin () {}
 
-    function handleAccountCreate () {
-      modal.open({
-        component: 'ModalAccountCreate'
-      })
-    }
+    function handleAccountCreate () {}
 
     return {
       handleLogin,
