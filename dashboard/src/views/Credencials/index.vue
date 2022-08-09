ee4c464a3b9efd26b8e45379362bb127312fd68d
Vanessa <vanessa-almeida@prodesan.com.br>
2022-08-09 10:24:37 -0300

    criando tela de credencias

--- dashboard/src/views/Credencials/index.vue
+++ dashboard/src/views/Credencials/index.vue
@@ -19,7 +19,17 @@
         Este aqui é a sua chave de api
       </p>
 
-      <div class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2">
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
+
+      <div
+        v-else
+        class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2"
+      >
         <span>{{ store.User.currentUser.apiKey }}</span>
         <div class="flex ml-20 mr-5">
           <icon
@@ -40,7 +50,16 @@
       <p class="mt-5 text-lg text-gray-800 font-regular">
         Coloque o script abaixo no seu site para começar a receber feedbacks
       </p>
+
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
+
       <div
+        v-else
         class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll"
       >
         <pre>
@@ -54,17 +73,23 @@
 </template>
 
 <script>
+import { reactive } from 'vue'
 import HeaderLogged from '../../components/HeaderLogged'
+import ContentLoader from '../../components/ContentLoader'
 import Icon from '../../components/Icon'
 import useStore from '../../hooks/useStore'
 import palette from '../../../palette'
 
 export default {
-  components: { HeaderLogged, Icon },
+  components: { ContentLoader, HeaderLogged, Icon },
   setup () {
     const store = useStore()
+    const state = reactive({
+      isLoading: false
+    })
 
     return {
+      state,
       store,
       brandColors: palette.brand
     }
