95ecba3738d6a0179dbbac4c3a92bbb8b5301c8f
Vanessa <vanessa-almeida@prodesan.com.br>
2022-08-12 15:42:05 -0300

    criando o Wizard de troca de estado

---
+++ widget/src/hooks/navigation.ts
@@ -0,0 +1,30 @@
+import useStore from './store'
+import { setCurrentComponent, setFeedbackType } from '../store'
+
+export interface Navigation {
+  next(): void
+  back(): void
+}
+
+export default function useNavigation (): Navigation {
+  const store = useStore()
+
+  function next (): void {
+    if (store.currentComponent === 'SelectFeedbackType') {
+      setCurrentComponent('WriteAFeedback')
+    }
+  }
+
+  function back (): void {
+    if (store.currentComponent === 'WriteAFeedback') {
+      setCurrentComponent('SelectFeedbackType')
+      // zera o feedbackType
+      setFeedbackType('')
+    }
+  }
+
+  return {
+    next,
+    back
+  }
+}
